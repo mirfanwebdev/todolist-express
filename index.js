3bdeced7e3da739d3ed0891192138c8af01d48fe
m.irfan <m.irfanwebdev@gmail.com>
2024-12-17 08:05:14 +0700

    add routes for CRUD function

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const todoRoutes = require("./routes/todo");
 
 const app = express();
 const port = 3000;
 
 // middleware
 app.use(express.json());
+
+// routes
+app.use("/api/todos", todoRoutes);
 
 // start server
 app.listen(port, () => {
